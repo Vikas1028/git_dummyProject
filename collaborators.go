25fc4080533a28f6c7c69428d87f81d74f223af0
aoshinbm <aoshinbm28797@gmail.com>
2023-03-10 11:32:49 +0530

    Refactor: Conflict raised

--- collaborators.go
+++ collaborators.go
@@ -5,33 +5,26 @@ import "fmt"
 func main() {
 	fmt.Println("Enter name of collaborators")
 
-<<<<<<< HEAD
 	fmt.Println(" BABU")
 	//merge conflict
-=======
+
+	fmt.Println("Enter name of collaborators")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
 	fmt.Println("AOSHIN BABU")
 
-	//trying merge conflict
->>>>>>> bran_AO
-
-
-fmt.Println("Enter name of collaborators")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("AOSHIN BABU")
-
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
-fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
 
 }
