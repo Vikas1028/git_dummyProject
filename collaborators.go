bae89c0267bfd101ea8f9839c6641db1131a34af
aoshinbm <aoshinbm28797@gmail.com>
2023-03-10 11:32:02 +0530

    Refactor: Conflict raised

--- collaborators.go
+++ collaborators.go
@@ -5,37 +5,33 @@ import "fmt"
 func main() {
 	fmt.Println("Enter name of collaborators")
 
-	//<<<<<<< HEAD
-	fmt.Println("collaborators name is AOSHIN conflict BABU")
-
-	//create merge conflic by aoshin
-
-	fmt.Println("Enter name of collaborators")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
+<<<<<<< HEAD
+	fmt.Println(" BABU")
+	//merge conflict
+=======
 	fmt.Println("AOSHIN BABU")
 
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
-	fmt.Println("This code change from Aoshin")
+	//trying merge conflict
+>>>>>>> bran_AO
+
+
+fmt.Println("Enter name of collaborators")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("AOSHIN BABU")
+
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+fmt.Println("This code change from Aoshin")
+
 }
