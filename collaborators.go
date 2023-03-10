9037040d14145705f7a77be1e443969c40ca3be2
aoshinbm <aoshinbm28797@gmail.com>
2023-03-10 11:28:00 +0530

    Refactor: Conflict raised

--- collaborators.go
+++ collaborators.go
@@ -4,6 +4,23 @@ import "fmt"
 
 func main() {
 	fmt.Println("Enter name of collaborators")
+
+	//<<<<<<< HEAD
+	fmt.Println("collaborators name is AOSHIN conflict BABU")
+
+	//create merge conflic by aoshin
+
+	fmt.Println("Enter name of collaborators")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
+	fmt.Println("This code change from Aoshin")
 	fmt.Println("This code change from Aoshin")
 	fmt.Println("This code change from Aoshin")
 	fmt.Println("This code change from Aoshin")
